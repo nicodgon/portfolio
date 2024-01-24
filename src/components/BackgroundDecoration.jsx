3bac3fe0634b49c93ec1fac63ec6ea18abfa9416
nico <nicolasdg19@hotmail.com>
2024-01-24 05:55:59 -0300

    translate

--- src/components/BackgroundDecoration.jsx
+++ src/components/BackgroundDecoration.jsx
@@ -2,7 +2,7 @@ export default function BackgroundDecoration(){
   return(
     <>
       <span className="bg-violet-700 w-40 h-40 absolute rounded-full translate-x-[23em] translate-y-[10em] blur z-10 animate-pulse md:max-xl:translate-y-[5em] md:max-xl:translate-x-[27em] max-md:translate-y-[32em] max-md:translate-x-[6em]"></span>
-      <span className="bg-violet-700 w-14 h-14 absolute rounded-full translate-x-[60em] translate-y-[46em] blur z-10 md:max-xl:translate-y-[30em] md:max-xl:translate-x-[50em] max-md:translate-y-[20em] max-md:translate-x-[2em] animate-pulse"></span>
+      <span className="bg-violet-700 w-14 h-14 absolute rounded-full translate-x-[60em] translate-y-[46em] blur z-10 md:max-xl:translate-y-[30em] md:max-xl:translate-x-[44em] max-md:translate-y-[20em] max-md:translate-x-[2em] animate-pulse"></span>
     </>
   )
 }
