65e225da72d5182aecf86a6a965165a46f1c5ae3
nico <nicolasdg19@hotmail.com>
2024-02-16 03:17:08 -0300

    profile

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -5,7 +5,7 @@ export default function ProfilePicture() {
     <div
       className={`w-64 h-64 rounded-full border-2 border-gray-800 overflow-hidden`}
     >
-      <img src={profile} alt="perfil" />
+      <img src={profile} alt="perfil" className="-translate-x-2 translate-y-3"/>
     </div>
   );
 }
